f427fdf226ab33068e3e94aa633fe8d262821415
Yves Trudeau <trudeau.y@gmail.com>
2018-10-04 21:44:39 -0400

    trying to square the urls 2

--- dicterm/urls.py
+++ dicterm/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^terme/',include('dicterm.terme.urls')),
+    url(r'^terme/',include('terme.urls')),
 )
