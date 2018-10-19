5e1dd4838870419130d22e918022cc9d39108bb8
Yves Trudeau <trudeau.y@gmail.com>
2018-10-18 21:02:54 -0400

    Essai davoir les noms de class specific dans ladmin

--- terme/models.py
+++ terme/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class Client(models.Model):
   nom_client = models.CharField(max_length=84)
   
