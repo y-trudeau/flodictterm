022b7673fc105adc2b089c66932cd52fc0f4e2e1
Yves Trudeau <trudeau.y@gmail.com>
2018-10-18 21:04:12 -0400

    Essai davoir les noms de class specific dans ladmin

--- terme/models.py
+++ terme/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from __future__ import unicode_literals
+from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
