3a80e1202865d0da278a7a7168ff1149fbf3ad6c
Yves Trudeau <y.trudeau@videotron.ca>
2018-10-23 21:55:04 -0400

    on ajoute la liste des termes

--- terme/urls.py
+++ terme/urls.py
@@ -3,7 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from terme.views import DomaineParClient
 
 urlpatterns = [
-    url(r'^Domaine/(?P<client>[0-9]+)/$', DomaineParClient.as_view()),
+    url(r'^Domaine/parClient/(?P<client>[0-9]+)/$', DomaineParClient.as_view()),
+    url(r'^ParDomaine/(?P<domaine>[0-9]+)/$', TermeParDomaine.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- terme/views.py
+++ terme/views.py
@@ -23,3 +23,15 @@ class DomaineParClient(APIView):
 		domaines = Domaine.objects.filter(client=client)
 		serializer = DomaineSerializer(domaines, many=True)
 		return Response(serializer.data)
+
+class TermeParDomaine(APIView):
+	def get(self, request, domaine):
+		try:
+			return TermeAn.objects.filter(domaine=domaine)
+		except TermeAn.DoesNotExist:
+			raise Http404
+	
+	def get(self, request, domaine, format=None):
+		termes = TermeAn.objects.filter(domaine=domaine)
+		serializer = TermeAnSerializer(termes, many=True)
+		return Response(serializer.data)
